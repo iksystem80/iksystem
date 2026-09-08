1b33f822a14f623291c4de9b9f99b5067a380eeb
Irfan Khowaja <i.khowaja@hotmail.com>
2026-09-08 13:52:11 -0500

    Update production API URL

--- assets/index-DitjiTYW.js
+++
@@ -1,1 +0,0 @@
-import{d as a,ai as o}from"./index-hUswG4K8.js";const n=a({created(){const{params:e,query:t}=this.$route,{path:r}=e;this.$router.replace({path:"/"+r,query:t})},render(){return o("div")}});export{n as default};
